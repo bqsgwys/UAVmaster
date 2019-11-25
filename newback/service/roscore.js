7443adbc0fb9a290ff76d3311caeae222dcb183a
武钰淞 <bqsgwys@outlook.com>
2019-11-25 16:04:18 +0800

    genx

--- newback/service/roscore.js
+++ newback/service/roscore.js
@@ -17,11 +17,42 @@ try {
         this.seenTar2.shutdown();
         this.seenTar3.shutdown();
         this.done.shutdown();
-        clearInterval(this.timer);
+        if (this.timer)
+          clearInterval(this.timer);
       }
     },
     init(nh) {
       this.start = 1;
+      this.target1 = nh.advertise(`/${g}/target1`, 'std_msgs/Int16');
+      this.target1.publish({
+        data: this.aim[0]
+      });
+      this.target2 = nh.advertise(`/${g}/target2`, 'std_msgs/Int16');
+      this.target2.publish({
+        data: this.aim[1]
+      });
+      this.target3 = nh.advertise(`/${g}/target3`, 'std_msgs/Int16');
+      this.target3.publish({
+        data: this.aim[2]
+      });
+      this.timer = setInterval(() => {
+        if (this.pub)
+          this.pub.publish({
+            data: 1
+          });
+        if (this.target1)
+          this.target1.publish({
+            data: this.aim[0]
+          });
+        if (this.target2)
+          this.target2.publish({
+            data: this.aim[1]
+          });
+        if (this.target3)
+          this.target3.publish({
+            data: this.aim[2]
+          });
+      }, 2000);
       this.takeoff = nh.subscribe(`/${g}/takeoff`, 'std_msgs/Int16', (msg) => {
         if (msg.data) {
           this.aim = gaim.mission.aims;
@@ -32,37 +63,8 @@ try {
             this.pub.publish({
               data: 1
             });
-            this.target1 = nh.advertise(`/${g}/target1`, 'std_msgs/Int16');
-            this.target1.publish({
-              data: this.aim[0]
-            });
-            this.target2 = nh.advertise(`/${g}/target2`, 'std_msgs/Int16');
-            this.target2.publish({
-              data: this.aim[1]
-            });
-            this.target3 = nh.advertise(`/${g}/target3`, 'std_msgs/Int16');
-            this.target3.publish({
-              data: this.aim[2]
-            });
-            this.timer = setInterval(() => {
-
-            console.log('mid')
-              this.pub.publish({
-                data: 1
-              });
-              this.target1.publish({
-                data: this.aim[0]
-              });
-              this.target2.publish({
-                data: this.aim[1]
-              });
-              this.target3.publish({
-                data: this.aim[2]
-              });
-            }, 2000);
           }
         }
-
       });
       this.seenfire = nh.subscribe(`/${g}/seenfire`, 'std_msgs/Int16', (msg) => {
         if (msg.data) player.seenFire(g);
